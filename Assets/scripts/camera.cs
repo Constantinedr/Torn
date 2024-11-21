cf818ff8d042c92c3e48577b4dd88c1add91b519
Constantinedr <kostalampadaris@gmail.com>
2024-11-21 14:26:13 +0200

    first

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraM : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.15f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // Calculate deltaX
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        // Calculate deltaY
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        // Update camera position
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
