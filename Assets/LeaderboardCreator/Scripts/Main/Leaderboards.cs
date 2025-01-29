aec8970f24d040a40e59b12f65a0901ea4e1d2e7
Constantinedr <kostalampadaris@gmail.com>
2025-01-29 16:39:17 +0200

    LEADERBOARD
    SDSDS

--- Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -2,6 +2,7 @@ namespace Dan.Main
 {
     public static class Leaderboards
     {
-        public static LeaderboardReference TornLeaderBoard = new LeaderboardReference("d5a45051562f80592eea8adc1e2ab3e5012e9b628f98f62482ce9716d7d44033 ");
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference TutorialLeaderboard = new LeaderboardReference("6e2f0277bbf77fd1cd232c6fa752bf2f2b5acc4f2d07ea377d8eee58f0348a2b");
     }
 }
